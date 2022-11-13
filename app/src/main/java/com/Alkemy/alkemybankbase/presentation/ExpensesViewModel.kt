43ca79f451eded48fd62dbb3c2042039a90ba469
MicaMathieu <myrcamathieu10@gmail.com>
2022-11-12 21:34:03 -0300

    MovementLayout

--- app/src/main/java/com/Alkemy/alkemybankbase/presentation/ExpensesViewModel.kt
+++ app/src/main/java/com/Alkemy/alkemybankbase/presentation/ExpensesViewModel.kt
@@ -7,25 +7,24 @@ import com.Alkemy.alkemybankbase.R
 import com.Alkemy.alkemybankbase.data.model.*
 import com.Alkemy.alkemybankbase.repository.account.AccountRepository
 import com.Alkemy.alkemybankbase.repository.expense.ExpensesRepository
-import com.Alkemy.alkemybankbase.repository.movement.MovementRepository
 import com.Alkemy.alkemybankbase.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.lang.IllegalArgumentException
+import java.util.regex.Pattern
 import javax.inject.Inject
 
 @HiltViewModel
 class ExpensesViewModel @Inject constructor(
-    private val expensesRepo : ExpensesRepository,
+    private val expensesRepo: ExpensesRepository,
     private val accountRepo: AccountRepository
-    ) : ViewModel() {
+) : ViewModel() {
 
-    val amountErrorResourceLiveData = MutableLiveData<Int>()
-    val conceptErrorResourceLiveData = MutableLiveData<String>()
-    val dateErrorResourceIdLiveData = MutableLiveData<String>()
-    val toAccountIdErrorResourceIdLiveData = MutableLiveData<Int>()
+    val amountErrorResourceLiveData = MutableLiveData<String>()
+    val conceptErrorLiveData = MutableLiveData<String>()
+//    val dateErrorResourceIdLiveData = MutableLiveData<String>()
+//    val toAccountIdErrorResourceIdLiveData = MutableLiveData<Int>()
     val isFormValidLiveData = MutableLiveData<Boolean>()
 
     lateinit var expenseResponse: Transaction
@@ -34,15 +33,28 @@ class ExpensesViewModel @Inject constructor(
     val errorExpenseLiveData = MutableLiveData<String>() //Error adding expense
     val isLoadingLiveData = MutableLiveData<Boolean>()
 
-    fun validateForm(concept:String,amount:Int,date:String,currency:String){
-        //TODO: Quien haga esta implementacion recuerde que currency no la vamos a usar, y que faltan parametros ahi
+    fun validateForm(concept: String, amount: Int, /*date: String, currency: String*/) {
+
+        val conceptPattern = "[a-zA-Z][a-zA-Z ]*"
+        val patternFn = Pattern.compile(conceptPattern)
+        val isConceptValid = patternFn.matcher(concept).matches()
+           if(!isConceptValid ){
+               conceptErrorLiveData.value = "Please enter a valid concept"
+               isFormValidLiveData.value = false
+           }else if(amount <= 0){
+               amountErrorResourceLiveData.value = "Please enter a valid amount"
+               isFormValidLiveData.value = false
+           }else{
+               isFormValidLiveData.value = true
+               conceptErrorLiveData.value = null
+               amountErrorResourceLiveData.value = null
+           }
     }
 
     fun addExpense(auth: String, amount: Int, concept: String, date: String, to_account_id: Int) {
         isLoadingLiveData.value = true
         getAllAccounts(auth)
-        if (!allAccountLiveData.value.isNullOrEmpty())
-        {
+        if (!allAccountLiveData.value.isNullOrEmpty()) {
             viewModelScope.launch(Dispatchers.Main) {
                 val response = withContext(Dispatchers.IO) {
                     val expense = Expense(
@@ -52,7 +64,8 @@ class ExpensesViewModel @Inject constructor(
                         "payment",
                         allAccountLiveData.value!!.first().id,
                         allAccountLiveData.value!!.first().userId,
-                    to_account_id)
+                        to_account_id
+                    )
                     expensesRepo.addExpense(auth, expense)
                 }
                 when (response) {
@@ -72,13 +85,13 @@ class ExpensesViewModel @Inject constructor(
         }
     }
 
-    fun getAllAccounts(auth: String){
+    fun getAllAccounts(auth: String) {
         isLoadingLiveData.value = true
-        viewModelScope.launch(Dispatchers.Main){
-            val response = withContext(Dispatchers.IO){
+        viewModelScope.launch(Dispatchers.Main) {
+            val response = withContext(Dispatchers.IO) {
                 accountRepo.getAllAccounts(auth)
             }
-            when(response){
+            when (response) {
                 is Resource.Failure -> {
                     isLoadingLiveData.value = false
                     errorLiveData.value = R.string.no_internet
@@ -87,7 +100,7 @@ class ExpensesViewModel @Inject constructor(
                 is Resource.Loading -> {
 
                 }
-                is Resource.Success ->{
+                is Resource.Success -> {
                     isLoadingLiveData.value = false
                     allAccountLiveData.value = response.data ?: emptyList()
                 }
--- app/src/main/java/com/Alkemy/alkemybankbase/ui/fragments/ExpenseFragment.kt
+++ app/src/main/java/com/Alkemy/alkemybankbase/ui/fragments/ExpenseFragment.kt
@@ -5,13 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.Alkemy.alkemybankbase.databinding.FragmentExpenseBinding
+import com.Alkemy.alkemybankbase.presentation.ExpensesViewModel
+import com.Alkemy.alkemybankbase.utils.afterTextChanged
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ExpenseFragment : Fragment() {
-    private var _binding : FragmentExpenseBinding? = null
+    private var _binding: FragmentExpenseBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: ExpensesViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,7 +23,46 @@ class ExpenseFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        _binding = FragmentExpenseBinding.inflate(inflater,container,false)
+        _binding = FragmentExpenseBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupObservers()
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        with(binding) {
+            etAmount.afterTextChanged {
+                viewModel.validateForm(
+                    etConcept.text.toString(),
+                    etAmount.text.toString().toIntOrNull() ?: 0
+                )
+            }
+            etConcept.afterTextChanged {
+                viewModel.validateForm(
+                    etConcept.text.toString(),
+                    etAmount.text.toString().toIntOrNull() ?: 0
+                )
+            }
+            btnAddExpense.isEnabled = false
+        }
+    }
+
+    private fun setupObservers() {
+        viewModel.isFormValidLiveData.observe(viewLifecycleOwner) {
+            binding.btnAddExpense.isEnabled = it
+        }
+
+        viewModel.conceptErrorLiveData.observe(viewLifecycleOwner) {
+            binding.etConcept.error = it
+        }
+        viewModel.amountErrorResourceLiveData.observe(viewLifecycleOwner) {
+            binding.etAmount.error = it
+        }
+    }
+
+
 }
